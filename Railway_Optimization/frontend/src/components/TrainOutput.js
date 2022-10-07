8488c00b9c1ea5686b23ceaf5c8f9ad129b76f6a
Saniya Inamdar <111994584+inamdarsaniya@users.noreply.github.com>
2022-10-07 12:13:05 +0530

    Update TrainOutput.js

--- Railway_Optimization/frontend/src/components/TrainOutput.js
+++ Railway_Optimization/frontend/src/components/TrainOutput.js
@@ -8,6 +8,7 @@ const TrainOutput=()=>{
     const [dest_time,setDestTime]=useState(null)
     const [duration,setDuration]=useState(null)
     const [path,setPath]=useState(null)
+    const [temp,setTemp]=useState(null)
     var tr
     
     
@@ -19,15 +20,22 @@ const TrainOutput=()=>{
             const response=await fetch("/api/trains/train")
             tr=await response.json()
             console.log(tr)
-            if(response.ok){
-                setSource(tr[0])
-                setSourceTime(tr[1])
-                setDest(tr[2])
-                setDestTime(tr[3])
-                setDuration(tr[4])
-                setPath(tr[5])
-
+            if(typeof tr=="object"){
+                if(response.ok){
+                    setSource(tr[0])
+                    setSourceTime(tr[1])
+                    setDest(tr[2])
+                    setDestTime(tr[3])
+                    setDuration(tr[4])
+                    setPath(tr[5])
+                    setTemp(tr)
+    
+                }
             }
+            if (typeof tr=="string"){
+                setTemp(tr)
+            }
+            
         }
         fetchDirectTrains()
 
@@ -36,23 +44,30 @@ const TrainOutput=()=>{
 
 
 
-
-    return(
-        <div className="trains">
-        <div className="ini">
-            <p><strong>Source:&nbsp;&nbsp;&nbsp;{src}</strong></p>
-            <p><strong>Time at Source: &nbsp;&nbsp;&nbsp;{source_time}</strong></p>
-            <p><strong>Destination: &nbsp;&nbsp;&nbsp;{dest}</strong></p>
-            <p><strong>Time at Destination: &nbsp;&nbsp;&nbsp;{dest_time}</strong></p>
-            <p><strong>{duration}</strong></p>
-
-        </div>
-        
-        {path && path.map((pat)=>(
-            <TrainDetails key={pat.key} pat={pat}/>
+    if(typeof temp=="object"){
+        return(
+            <div className="trains">
+            <div className="ini">
+                <p><strong>Source:&nbsp;&nbsp;&nbsp;{src}</strong></p>
+                <p><strong>Time at Source: &nbsp;&nbsp;&nbsp;{source_time}</strong></p>
+                <p><strong>Destination: &nbsp;&nbsp;&nbsp;{dest}</strong></p>
+                <p><strong>Time at Destination: &nbsp;&nbsp;&nbsp;{dest_time}</strong></p>
+                <p><strong>{duration}</strong></p>
+    
+            </div>
             
-        ))}
-    </div>
-    )
+            {path && path.map((pat)=>(
+                <TrainDetails key={pat.key} pat={pat}/>
+                
+            ))}
+        </div>
+        )
+    }
+    if (typeof temp=="string"){
+        return(
+            <h2>No direct trains available</h2>
+        )
+    }
+    
    }
    export default TrainOutput 
